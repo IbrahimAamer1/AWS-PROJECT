2819e63906ed2f00a62106b34a44b4e2ba584dbd
IbrahimAamer1 <ibrahimaamer115@gmail.com>
2024-09-26 16:22:08 +0300

    Update ASG.tf

--- ASG.tf
+++ ASG.tf
@@ -2,17 +2,17 @@ resource "aws_launch_configuration" "app" {
   name                 = "app-launch-configuration"
   image_id             = "ami-0e04bcbe83a83792e"
   instance_type        = "t2.micro"
-  key_name             = "key"
+  key_name             = aws_key_pair.UbuntuKP.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   security_groups      = [aws_security_group.bastion.id]
   user_data            = <<-EOF
 #!/bin/bash
-yum update -y
-yum install -y python3
-echo "Hello, World from ASG , $(hostname -f) " > /home/ec2-user/index.html
-cd /home/ec2-user
-python3 -m http.server 80 &
+sudo apt update -y
+sudo apt install -y python3
+echo "Hello, World from ASG, $(hostname -f)" | sudo tee /var/www/html/index.html
+sudo python3 -m http.server 80 --directory /var/www/html/ &
 EOF
+
 }
 resource "aws_autoscaling_group" "app" {
   name                 = "ASG"
